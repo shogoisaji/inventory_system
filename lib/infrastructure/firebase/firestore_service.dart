a6e121ea682cdf1b4c9dd5603370adf824bb06ac
j601u03 <j601u03@J601.kansai-center.ac.jp>
2023-07-28 16:55:58 +0900

    change

--- lib/infrastructure/firebase/firestore_service.dart
+++ lib/infrastructure/firebase/firestore_service.dart
@@ -1,25 +1,41 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/material.dart';
 
-import '../../presentation/pages/warehousing_page.dart';
 import '../../presentation/wedget/date_get.dart';
 
 class FireStoreService {
   final db = FirebaseFirestore.instance;
 
   String currentDate = getCurrentDate();
-  /// サインイン
   Future<void> upLoad(String productName, String productId, String productType, String productVolume) async {
+    int? nextId = int.tryParse(getNextId());
     await db
         .collection('items')
-        .doc('product')
+        .doc('product$nextId')
         .set({
           'productName': productName,
-          'productId': productId,
           'productType': productType,
-          'productVolume' : productVolume,
+          'productId': getNextId(),
+          'productVolume' : int.tryParse(productVolume),
           'timeStamp': currentDate,
-        }); 
+        }); // imageトークンを入れるよ良い
   }
+
+Future<String> getNextId() async {
+  QuerySnapshot snapshot = await db
+      .collection('items')
+      .orderBy('productId', descending: true) // 'id'フィールドで降順ソート
+      .limit(1) // 最後の1件だけ取得
+      .get();
+
+  if (snapshot.docs.isEmpty) {
+    // コレクションが空の場合、最初のIDを"1"とします
+    return "1";
+  } else {
+    // 最後のドキュメントのIDを取得し、1加えて文字列に戻します
+    int lastId = int.parse(snapshot.docs.first.data()!['productId']);
+    return (lastId + 1).toString();
+  }
+}
+
 
 }
